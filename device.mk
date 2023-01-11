4fbdd49f116818cdeb6956542f9dcfca428a43d9
Your Name <you@example.com>
2023-01-11 19:24:27 +0530

    initial

---
+++ device.mk
@@ -0,0 +1,2 @@
+LOCAL_PATH := device/tecno/KE6
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
---
+++ twrp_KE6.mk
@@ -0,0 +1,16 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from KE6 device
+$(call inherit-product, device/tecno/KE6/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := KE6
+PRODUCT_NAME := twrp_KE6
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := KE6
+PRODUCT_MANUFACTURER := Tecno
+PRODUCT_RELEASE_NAME := Tecno KE6
