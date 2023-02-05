c2b24e2901f64610fc3dfcd061d031f8cdedbda8
Dhruv <122255891+dhruvtadvi@users.noreply.github.com>
2023-02-05 16:57:44 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
+MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
+DEVICE=phoenix
+DT_LINK="https://github.com/hraj9258/twrp_phoenix -b a11"
+DT_PATH=device/xiaomi/$DEVICE
+
+echo " ===+++ Setting up Build Environment +++==="
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir ~/twrp11 && cd ~/twrp11
+
+echo " ===+++ Syncing Recovery Sources +++==="
+repo init --depth=1 -u $MANIFEST
+repo sync
+repo sync
+git clone --depth=1 $DT_LINK $DT_PATH
+
+echo " ===+++ Building Recovery +++==="
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+echo " source build/envsetup.sh done"
+lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
+
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+
+cd out/target/product/$DEVICE
+mv recovery.img ${OUTFILE%.zip}.img
+zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+
+curl -T $OUTFILE https://oshi.at
